efab0a0f591508eb9fd5e8956ad7e2f46ca2a5fb
AMAN PARASHAR <107232134+Amanparashar9@users.noreply.github.com>
2022-07-12 19:03:31 +0530

    Add files via upload

---
+++ food blogging/places.js
@@ -0,0 +1,27 @@
+function search_places() {
+    var x = document.getElementById("search").value;
+    if (x === "karol bagh") {
+        window.open("/karolbagh.html")
+    }
+    if (x === "chandani chowk") {
+        window.open("/chandnichowk.html")
+    }
+    if (x === "jama masjid") {
+        window.open("/jamamasjid.html")
+    }
+    if (x === "kamla nagar") {
+        window.open("/kamlanagar.html")
+    }
+    if (x === "krishna nagar") {
+        window.open("/krishnanagar.html")
+    }
+    if (x === "lajat nagar") {
+        window.open("/lajatnagar.html")
+    }
+    if (x === "shahdara") {
+        window.open("/shahdara.html")
+    }
+    if (x === "connaught place") {
+        window.open("/connaughtplace.html")
+    }
+}
